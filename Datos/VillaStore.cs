d08c42cb1da58f10e8728c4efd21eb07318ec808
ArquimedesGR <gabox2050@gmail.com>
2023-09-30 18:04:07 -0300

    making endpoints

---
+++ Datos/VillaStore.cs
@@ -0,0 +1,15 @@
+﻿using ApiVillaC.Controllers.Models.Dto;
+
+namespace ApiVillaC.Datos
+{
+    public static class VillaStore
+    {
+
+        public static List<VillaDto> VillaList = new List<VillaDto>()
+
+        {
+            new VillaDto{Id= 1, Name="Vista a la Piscina"},
+            new VillaDto{Id= 2, Name="Vista a la Playa"}
+        };
+    }
+}
